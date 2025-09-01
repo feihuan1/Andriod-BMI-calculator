7cfae7ff5c14af8ade3790935fffbbf926321c6f
Feihuan Peng <feihuan.peng@spreetail.com>
2025-08-31 22:25:52 -0500

    asd

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.bmicalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +17,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String alertText = "OMG, I can code in Java";
+        TextView resutText = findViewById(R.id.text_view_result);
 
-        Toast.makeText(this, alertText, Toast.LENGTH_LONG).show();
+        RadioButton radioButtonMale = findViewById(R.id.radio_button_male);
+        RadioButton radioButtonFemale = findViewById(R.id.radio_button_female);
 
+        EditText editTextAge = findViewById(R.id.edit_text_age);
+        EditText editTextFeet = findViewById(R.id.edit_text_feet);
+        EditText editTextInches = findViewById(R.id.edit_text_inches);
+        EditText editTextweight = findViewById(R.id.edit_text_weight);
+
+        Button calculateButton = findViewById(R.id.button_calculate);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "clicked",Toast.LENGTH_LONG).show();
+            }
+        });
+
+//        resutText.setText("Woohoo, Updated");
+//        String alertText = "OMG, I can code in Java";
+//        Toast.makeText(this, alertText, Toast.LENGTH_LONG).show();
     }
 }
